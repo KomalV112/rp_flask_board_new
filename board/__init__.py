7eefbbf94cb5c75ed0e13890413aa3e10b5bd5e6
Komal1198 <123831443+KomalV112@users.noreply.github.com>
2025-06-28 14:52:44 -0400

    Commit message

--- board/__init__.py
+++ board/__init__.py
@@ -1,13 +1,19 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from board import pages,posts,database
 
-
-from flask import Flask, render_template
-from board import pages
+load_dotenv()
 
 
 def create_app():
     app=Flask (__name__)
+    app.config.from_prefixed_env()
 
+    database.init_app(app)
     app.register_blueprint(pages.bp)
-
+    app.register_blueprint(posts.bp)
+    print(f"Current Enviornment:{os.getenv('ENVIORNMENT')}")
+    print(f"Database Path:{app.config.get('DATABASE')}")
     return app
 
